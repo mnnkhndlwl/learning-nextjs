e1a35b56b58d5f2f7a6d71e8aaa6f0a61f44af16
mnnkhndlwl <khandelwalmanan05@gmail.com>
2023-03-15 23:31:36 +0530

    routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
-import styles from '@/styles/Home.module.css'
+import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
+  var user = "mnnkhndlwl";
   return (
     <>
       <div className={styles.main}>
-        <h1>
-        hello 
-        </h1>
+        <Link
+          href={{
+            pathname: `profile/[username]`,
+            query: {
+              username: user,
+            },
+          }}
+        >
+          About
+        </Link>
+        <h1>hello</h1>
       </div>
     </>
-  )
+  );
 }
